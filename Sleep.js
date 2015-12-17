d665c00054082fc7fef074541920f540fab20a57
AlbanT <alban@reptile-addict.nl>
2015-12-17 14:25:47 +0100

    added sleep.js
    a function to wait for a set amount of milliseconds before continuing

---
+++ Sleep.js
@@ -0,0 +1,12 @@
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds){
+			break;
+		}
+	}
+}
+
+alert("now");
+sleep(5000);
+alert("5 seconds later");
