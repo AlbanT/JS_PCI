d96d0fa87c54bc1aa315e1f378294768d6e0049c
Alban <AlbanT@users.noreply.github.com>
2019-12-24 15:52:20 +0100

    Update ShankCsvCreator.js
    update to use with both drills and mills

--- Tooltjes/ShankCsvCreator.js
+++ Tooltjes/ShankCsvCreator.js
@@ -9,18 +9,43 @@ Description		:	Small PCI script for generating simple shanks for edgecam tools a
 	
 Disclaimer		:	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+var CSVfolder;
 
 // build the dialog
 var hOp = InitOperation("Shank Creator", "", 0);
-AddUserModToOperation(hOp, "$ShankCreator[7]", "Name of csv", "^", 0, "");
-AddUserModToOperation(hOp, "$ShankCreator[3]", "flute length", "^", 0, "");
 
-AddUserModToOperation(hOp, "$ShankCreator[1]", "clearance diameter", "^clearance", 0, "");
-AddUserModToOperation(hOp, "$ShankCreator[4]", "clearance length", "^clearance", 0, "");
-AddUserModToOperation(hOp, "$ShankCreator[5]", "length to top of chamfer", "^clearance", 0, "");
+    //initialize "add milling tool"
+    var CmdTstoreHole = InitCommand(36, 108); 
+    ClearMods(CmdTstoreHole); 
+    SetModifier(CmdTstoreHole, 140, "hole|1");
 
-AddUserModToOperation(hOp, "$ShankCreator[2]", "shank diameter", "^shank", 0, "");
-AddUserModToOperation(hOp, "$ShankCreator[6]", "total tool length", "^shank", 0, "");
+    var CmdTstoreMill = InitCommand(36, 108); 
+    ClearMods(CmdTstoreMill); 
+    SetModifier(CmdTstoreMill, 140, "mill|0");
+
+    // add the toolstore browse button to the operation
+    var nRet=AddCmdModToOperation(hOp, CmdTstoreHole, 180 , "^TSTORE" , 0 , "");
+
+    //Sets up ToolStore to be ready to return values into modifiers contained in the PCI-JS
+    SetCallBack("tsHoleTool" ,hOp  , CmdTstoreHole, 180);
+
+    // add the toolstore browse button to the operation
+    var nRet=AddCmdModToOperation(hOp, CmdTstoreMill, 180 , "^TSTORE" , 0 , "");
+
+    //Sets up ToolStore to be ready to return values into modifiers contained in the PCI-JS
+    SetCallBack("tsMillTool" ,hOp  , CmdTstoreMill, 180);
+    SetCallBack("tsHoleTool" ,hOp  , CmdTstoreHole, 180);
+
+    AddUserModToOperation(hOp, "_check_ShankCreator", "drill", "^", 0, "");
+
+    AddUserModToOperation(hOp, "$ShankCreator[1]", "clearance diameter", "^clearance", 0, "");
+    AddUserModToOperation(hOp, "$ShankCreator[4]", "clearance length", "^clearance", 0, "");
+    AddUserModToOperation(hOp, "$ShankCreator[5]", "length to top of chamfer", "^clearance", 0, "");
+
+    AddValidStateCmdMod(hOp, "_check_ShankCreator", 1, CmdTstoreMill, 180, 0);
+    AddValidStateCmdMod(hOp, "_check_ShankCreator", 1, CmdTstoreHole, 180, 1);
+    AddValidStateCmdMod(hOp, "_check_ShankCreator", 0, CmdTstoreMill, 180, 1);
+    AddValidStateCmdMod(hOp, "_check_ShankCreator", 0, CmdTstoreHole, 180, 0);
 
 var nOpRet = DoOperationMods(hOp);
 FreeOperation(hOp);
@@ -30,15 +55,49 @@ if (nOpRet == _FINISH) {
 
     // retrieve the values from the dialog and convert them into usable values
     var D2 = 0.5 * GetPCINumber("$ShankCreator[1]");        // clearance diameter
-    var D3 = 0.5 * GetPCINumber("$ShankCreator[2]");        // shank diameter
 
-    var L = GetPCINumber("$ShankCreator[3]");               // flute length
     var L1 = GetPCINumber("$ShankCreator[4]");              // clearance length
     var L2 = GetPCINumber("$ShankCreator[5]");              // length to top of chamfer
-    var L3 = GetPCINumber("$ShankCreator[6]");              // total tool length
-    var ToolName = GetPCIVariable("$ShankCreator[7]");      // total tool length
+   
+    if (GetPCINumber("_check_ShankCreator") == 1) {
+        // extract shankdiameter from selected tool
+        GetModifier(CmdTstoreHole, 180, "$TEMP");
+        ToolName = GetPCIVariable("$TEMP");
 
-    var CSVfolder = GetRegistryString(_TSTORE_SUP_DIR_CFG_REC) + "ToolingGraphics\\Mill";
+        // extract shankdiameter from selected tool
+        GetModifier(CmdTstoreHole, 186, "TEMP");
+        D3 = 0.5 * GetPCINumber("TEMP");
+
+        // extract flute length from selected tool
+        GetModifier(CmdTstoreHole, 179, "TEMP");
+        L = GetPCINumber("TEMP");
+
+        // extract total tool length from selected tool
+        GetModifier(CmdTstoreHole, 185, "TEMP");
+        L3 = L + GetPCINumber("TEMP");
+
+        CSVfolder = GetRegistryString(_TSTORE_SUP_DIR_CFG_REC) + "ToolingGraphics\\Hole";
+    }
+    else {
+        // extract shankdiameter from selected tool
+        GetModifier(CmdTstoreMill, 180, "$TEMP");
+        ToolName = GetPCIVariable("$TEMP");
+
+        // extract shankdiameter from selected tool
+        GetModifier(CmdTstoreMill, 186, "TEMP");
+        D3 = 0.5 * GetPCINumber("TEMP");
+
+        // extract flute length from selected tool
+        GetModifier(CmdTstoreMill, 179, "TEMP");
+        L = GetPCINumber("TEMP");
+
+        // extract total tool length from selected tool
+        GetModifier(CmdTstoreMill, 185, "TEMP");
+        L3 = L + GetPCINumber("TEMP");
+
+        CSVfolder = GetRegistryString(_TSTORE_SUP_DIR_CFG_REC) + "ToolingGraphics\\Mill";
+    }
+	
 
     myFile = CSVfolder + "\\" + ToolName + "_shank.csv";
 
@@ -64,5 +123,8 @@ if (nOpRet == _FINISH) {
         f.writeline("OFFSET,0.000000,0.000000," + (L2 + 5));
         f.Close();
     }
+	
+	Display("created:\t" + ToolName + "_shank.csv\n");
 }
-
+FreeCommand(CmdTstoreHole);  // release initialized command
+FreeCommand(CmdTstoreMill);  // release initialized command
